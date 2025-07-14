2b2646e9252c92bd1a9edd837bbfee503a471618
ParkJunhyun0304 <162578133+ParkJunhyun0304@users.noreply.github.com>
2025-07-14 16:43:31 +0900

    [Solve] Problem A055

---
+++ easy/A055_박준현_20250714.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int open = 0;
+        string result = "";
+        for(int i=0; i<s.size(); i++) {
+            if(s[i] == '(') {
+                if(open > 0) result += "(";
+                open ++;
+            } else if(s[i] == ')') {
+                open --;
+                if(open > 0) result += ")";
+            }
+        }
+
+        return result;
+    }
+};
