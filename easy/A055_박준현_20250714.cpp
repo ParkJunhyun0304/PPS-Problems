bba9bd5cc5127d5325190b57618388fd8494fee5
ParkJunhyun0304 <162578133+ParkJunhyun0304@users.noreply.github.com>
2025-07-20 12:15:00 +0900

    [Solve] Problem A055

--- easy/A055_박준현_20250714.cpp
+++ easy/A055_박준현_20250714.cpp
@@ -1,38 +1,23 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
+#include <string>
+#include <vector>
 
-// cookie_len은 배열 cookie의 길이입니다.
-int solution(int cookie[], size_t cookie_len) {
-    int answer = -1;
-    
-    int left = 0, right = 0;
-    
-    for(int i=0; i<cookie_len-1; i++) {
-        left = i;
-        right = left+1;
-        
-        int left_sum = cookie[left];
-        int right_sum = cookie[right];
-        
-        while(left >= 0 && right < cookie_len) {
-            if(left_sum == right_sum) {
-                if(answer < left_sum) {
-                    answer = left_sum;
-                }
-            }
-            if(left_sum <= right_sum) {
-                left --;
-                if(left >= 0)
-                    left_sum += cookie[left];
-            } else if(left_sum > right_sum) {
-                right++;
-                if(right < cookie_len)
-                    right_sum += cookie[right];
+using namespace std;
+
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int open = 0;
+        string result = "";
+        for(int i=0; i<s.size(); i++) {
+            if(s[i] == '(') {
+                if(open > 0) result += "(";
+                open ++;
+            } else if(s[i] == ')') {
+                open --;
+                if(open > 0) result += ")";
             }
         }
+
+        return result;
     }
-    if(answer == -1) answer = 0;
-    
-    return answer;
-} 
+};
