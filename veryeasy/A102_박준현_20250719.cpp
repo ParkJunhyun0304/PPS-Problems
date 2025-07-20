51bc32c4a9d78f6c9c94a96a7954bb7a101bee83
ParkJunhyun0304 <162578133+ParkJunhyun0304@users.noreply.github.com>
2025-07-20 23:13:40 +0900

    [Solve] A102

---
+++ veryeasy/A102_박준현_20250719.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+
+        vector<vector<int>> result(matrix[0].size(), vector<int>(matrix.size()));
+        for(int i=0; i<matrix.size(); i++) {
+            for(int j=0; j<matrix[i].size(); j++) {
+                result[j][i] = matrix[i][j];
+            }
+        }
+        return result;
+    }
+};
